b75b6d4c7dadb7b25be421d18a840bbd97841263
Diego <dipeoliver@outlook.com>
2021-06-19 09:00:24 -0300

    New project to learn RecyclerView

---
+++ WhatDidILearn/app/src/main/java/com/example/whatdidilearn/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.whatdidilearn
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.whatdidilearn.data.LearnedItemDatabase
+import com.example.whatdidilearn.view.LearnedItemAdapter
+
+lateinit var binding: ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val recycler = binding.recyclerView
+        val adapter = LearnedItemAdapter()
+        adapter.learnedItems = LearnedItemDatabase.getAll()
+        recycler.adapter = adapter
+
+
+
+    }
+}
---
+++ WhatDidILearn/app/src/main/java/com/example/whatdidilearn/data/UnderstandingLevel.kt
@@ -0,0 +1,7 @@
+package com.example.whatdidilearn.data
+
+enum class UnderstandingLevel {
+    LOW,
+    MEDIUM,
+    HIGH
+}
