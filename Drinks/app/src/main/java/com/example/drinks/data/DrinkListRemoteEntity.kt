ebc8783d3695f53d767c61e022b61094896d17cb
Diego <dipeoliver@outlook.com>
2021-06-26 16:16:22 -0300

    update App folders

--- Drinks/app/src/main/java/com/example/drinks/data/DrinkListRemoteEntity.kt
+++
@@ -1,9 +0,0 @@
-package com.example.drinks.data
-
-import com.google.gson.annotations.SerializedName
-
-data class DrinkListRemoteEntity(
-    // o serialized faz a converção do nome
-    @SerializedName("drinks")
-    val drinksList: List<DrinkRemoteEntity>
-)
--- Drinks/app/src/main/java/com/example/drinks/data/DrinkRemoteEntity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.drinks.data
-
-data class DrinkRemoteEntity(
-    val idDrink: String,
-    val strDrink: String,
-    val strDrinkThumb: String
-)
